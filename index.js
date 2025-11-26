0c716aadf8bfc8ee4a3d7c0b9f1375e42e458e9a
ruipsl95 <ruipedro.babo@hotmail.com>
2025-11-26 09:07:49 +0000

    Atualização de rota com implementação do padrão

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-
+const DeployController = require('./src/controllers/DeployController'); 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -38,6 +38,8 @@ app.post('/analytics_url', (req, res) => {
     res.sendFile(path.join(__dirname, 'data/analytics_test.json'));
 });
 
+//Rota que implementa o padrão
+app.get('/user_url', DeployController.handleDeploy);
 
 app.get('/', (req, res) => {
     res.send('<h1>O Servidor CodeQuest está a correr! </h1><p>Rotas:</p><p> <a href="/config_url">/config_url</a></p><p> <a href="/json_params_url">/json_params_url</a></p><p> <a href="/analytics_list_url">/analytics_list_url</a></p><p> <a href="/user_url?invenira_activity_id=teste123">/user_url?invenira_activity_id=teste123</a></p><p> <a href="/analytics_url">/analytics_url</a> (POST)</p>');
@@ -46,3 +48,7 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Servidor CodeQuest online http://localhost:${PORT}`);
 });
+
+
+
+
